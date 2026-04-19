40823f0f20d91a0cf3605d28063904fc821e60da
SignpostMarv <me@signpostmarv.name>
2026-04-19 20:51:31 +0000

    adjusting indentation

--- tests/fixtures/Object.entries.fail.ts
+++ tests/fixtures/Object.entries.fail.ts
@@ -1,13 +1,19 @@
 (() => {
-const foo: {
-	[key: `${string}_${string}`]: `${string}:${number}`,
-} = {
-	foo_bar: 'baz:1',
-};
+	const foo: {
+		[key: `${string}_${string}`]: `${string}:${number}`,
+	} = {
+		foo_bar: 'baz:1',
+	};
 
-function typed([key, value]: [`${string}_${string}`, `${string}:${number}`]) {
-	console.log(key, value);
-}
+	function typed([
+		key,
+		value,
+	]: [
+		`${string}_${string}`,
+		`${string}:${number}`,
+	]) {
+		console.log(key, value);
+	}
 
-Object.entries(foo).map((e) => typed(e));
+	Object.entries(foo).map((e) => typed(e));
 })();
--- tests/fixtures/Object.keys.fail.ts
+++ tests/fixtures/Object.keys.fail.ts
@@ -1,13 +1,13 @@
 (() => {
-const foo: {
-	[key: `${string}_${string}`]: `${string}:${number}`,
-} = {
-	foo_bar: 'baz:1',
-};
+	const foo: {
+		[key: `${string}_${string}`]: `${string}:${number}`,
+	} = {
+		foo_bar: 'baz:1',
+	};
 
-function typed(key: `${string}_${string}`) {
-	console.log(key);
-}
+	function typed(key: `${string}_${string}`) {
+		console.log(key);
+	}
 
-Object.keys(foo).map((e) => typed(e));
+	Object.keys(foo).map((e) => typed(e));
 })();
